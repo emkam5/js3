99fb010dafe012f72da462671055eec029715213
emkam5 <wertysedt@gmail.com>
2024-09-19 23:40:25 +0300

    js3

---
+++ assets/js/main.js
@@ -0,0 +1,68 @@
+'use strict';
+
+// 1
+
+let cost = +prompt('Стоимость товара');
+let summ = +prompt('Количество денег клиента');
+let gg = 0;
+
+if ((cost > 0) && (summ > 0)) {
+    if (cost > summ) {
+        gg = cost - summ;
+        console.log(`Нехватает ${gg} р.`);
+    } else if (cost < summ) {
+        gg = summ - cost;
+        console.log(`Покупка совершена. Ваша сдача ${gg} р.`);
+    } else if (cost === summ) { // Fixed equality check
+        console.log('Покупка совершена');
+    }
+} else {
+    alert('Некорректные данные');
+}
+
+// 2
+
+let num = +prompt("Введите число");
+
+if (num > 0) {
+    alert('1');
+} else if (num < 0) {
+    alert('-1');
+} else if (num === 0) { // Use strict equality
+    alert('0');
+}
+
+// 3
+
+let a = Number(prompt("Введите число"));
+let b = Number(prompt("Введите число"));
+
+let result = (a + b < 4) ? 'Мало' : 'Много';
+console.log(result);
+
+// 4
+
+let l = prompt("Введите логин");
+let message = (l === 'Сотрудник') ? 'Привет' :
+              (l === 'Директор') ? 'Здравствуйте' :
+              (l === '') ? 'Нет логина' : 'Неверно';
+
+console.log(message);
+
+// 5
+
+let login = prompt("Ваш логин");
+if (login === "Админ") { // Use strict equality
+    let password = prompt("Пароль");
+    if (password === 'Я главный') { // Use strict equality
+        console.log('Здравствуйте!');
+    } else if(password === null) { // Use strict equality
+        console.log('Отменено');
+    } else {
+        console.log('Я вас не знаю');
+    }
+} else if (login === null) { // Use strict equality
+    console.log("Отменено");
+} else {
+    console.log("Я вас не знаю");
+}
